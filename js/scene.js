dec23b7a0ff09b384b54aa70b3bc74cf389b0fcb
raged07 <i07villalobos@gmail.com>
2017-11-13 01:21:40 -0700

    Changing textures

--- js/scene.js
+++ js/scene.js
@@ -12,36 +12,20 @@
         camera.position.z = 10;
         camera.position.y = 130;
         camera.rotation.x = -1.5;
-//////////////////////PLANO////BACKGROUND/////////////////////
-        let planeGeometry = new THREE.PlaneGeometry(300,600);
+
+        let loader = new THREE.TextureLoader();
+/////////////////// PLANO////BACKGROUND ///////////////////////////
+    loader.load('public/basket.jpg', function(texture){
+        let planeGeometry = new THREE.PlaneGeometry(300,250);
         planeGeometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI/2));
         let groundMaterial = new THREE.MeshPhongMaterial({
-            color: 0x00ffff,
+            map: texture,
             side: THREE.DoubleSide
             });
         let plane = new THREE.Mesh(planeGeometry, groundMaterial);
         plane.receiveShadow  = true;
-        let mesh;
-    
-        let loader = new THREE.TextureLoader();
-////////////////////////// ESFERA //////////////////////////
-    loader.load('public/map1.jpg', function(texture){
-            let geometry = new THREE.SphereGeometry(15,50,50)
-            let material = new THREE.MeshBasicMaterial({
-                //color: 0xffff00,
-                map: texture
-                , wireframe: true
-            })
-    
-            mesh = new THREE.Mesh(geometry, material);
-    
-            mesh.position.y = 25;
-            mesh.position.z = 40;
-
-            mesh.castShadow = true;
-           // scene.add(mesh);
-        })
-
+        scene.add(plane);
+        });
 //////////////////////// CUBO ////////////////////////////////
         let geometry = new THREE.BoxGeometry(20,20,20,20);
         let groundMaterialC = new THREE.MeshPhongMaterial({
@@ -53,20 +37,24 @@
 
 ////////////// TOROIDE (DONA xD) ////////////////////////////
        let geometryT = new THREE.TorusGeometry( 15, 5, 20, 100 );
-       let material = new THREE.MeshBasicMaterial( { 
+       let material = new THREE.MeshPhongMaterial({ 
            color: 0xffff00 
            //,wireframe: true
         } );
        let torus = new THREE.Mesh( geometryT, material );
-       torus.castShadow = true;
+       
+        torus.castShadow = true;
+        torus.castShadow = true;
 
 ////////////////////// PIRAMIDE //////////////////////////////
         let geometryP = new THREE.CylinderGeometry( 0, 20, 20, 4 );
-        let materialP = new THREE.MeshBasicMaterial( {
-            color: 0xff00ff 
+        let materialP = new THREE.MeshPhongMaterial( {
+            color: 0x00ffff 
             //, wireframe:true
         } );
         let pyramid = new THREE.Mesh( geometryP, materialP );
+        let c=0;
+        
         pyramid.receiveShadow = true;
         pyramid.castShadow = true;
 ///////////////////// LUZ //////////////////////////////////
@@ -89,16 +77,34 @@
     
         scene.background = new THREE.Color(0xeeeeee);
         scene.add(new THREE.AmbientLight(0x505050));
-        scene.add(plane);
+        
         scene.add(cube);
         scene.add(torus);
         scene.add(pyramid);
         
         scene.add(pointLight);
-    
+////////////////// Cambiar Textura ////////////////////////
+        let changer = 0;
+        document.body.onkeypress= function(){  // Dectecta si se presiona alguna tecla
+         if(changer % 2 == 0){                 // Se inicializa una funcion que determina el residuo 
+                                               //para alternar entre 2 valores y cargar, ya sea, una u otra textura
+             texture = new THREE.TextureLoader().load('../public/lava.jpg');
+            }
+         else{
+             texture = new THREE.TextureLoader().load('../public/water.gif');
+            }
+                                                // Se alterna entre las dos texturas al oprimir cualquier tecla
+                                                // Se carga la textura a las 3 primitivas
+            torus.material = new THREE.MeshBasicMaterial({map:texture})
+            pyramid.material = new THREE.MeshBasicMaterial({map:texture})
+            cube.material = new THREE.MeshBasicMaterial({map:texture})
+            renderer.render(scene, camera);
+                                                // Aumentamos el contador para que haya alternancia
+            changer += 1;
+         }
+                                                //Controles de orbita, sirve para mover la camara
         let controls = new THREE.OrbitControls(camera, renderer.domElement);
-    
-    
+
         function loop(){
             requestAnimationFrame(loop);
             cube.rotation.y += 0.01;
